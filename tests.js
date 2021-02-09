ebecb14d49c6ecdc2a94568bc3eba4c771425592
John Pierce <pierce.johndaniel@gmail.com>
2021-02-09 12:35:37 -0600

    add the second test and solution for intro-to-testing

--- tests.js
+++ tests.js
@@ -18,4 +18,13 @@ describe("sayHello", function (){
     it("should be a defined function", function () {
         expect(typeof sayHello).toBe("function");
     });
+    it("should return a string when called", function() {
+        expect(typeof sayHello()).toBe("string");
+    });
+    it("should return the string 'Hello' when executed", function() {
+        expect(sayHello()).toBe("Hello");
+    });
+    it("should never retrun 'undefined' when called", function () {
+        !expect(sayHello()).not.toBe(undefined);
+    });
 })
